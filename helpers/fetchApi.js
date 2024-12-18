81467218bd2e7dfab08d6777ffa31d49c9c638b8
Pedro Domínguez Aguilera <p.dominguezaguilera@ext.douglas.eu>
2024-12-18 19:34:39 +0100

    Adding new headers

--- helpers/fetchApi.js
+++ helpers/fetchApi.js
@@ -3,8 +3,17 @@ const fetchApi = async (url) => {
     const res = await fetch(url, {
       headers: {
         "User-Agent":
-          "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:100.0) Gecko/20100101 Firefox/100.0",
-        connection: "keep-alive",
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Accept-Encoding": "gzip, deflate, br",
+        Accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        Referer: url,
+        Connection: "keep-alive",
+        DNT: "1", // Do Not Track request header
+        "Upgrade-Insecure-Requests": "1",
+        "Cache-Control": "no-cache",
+        Pragma: "no-cache",
       },
     });
 
