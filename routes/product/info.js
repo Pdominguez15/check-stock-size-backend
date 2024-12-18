31a5aaac372035008ab7c87fdc1e5514d112f39c
Pedro Domínguez Aguilera <p.dominguezaguilera@ext.douglas.eu>
2024-12-18 19:24:03 +0100

    Improve errors messages

--- routes/product/info.js
+++ routes/product/info.js
@@ -3,18 +3,21 @@ import getProductUrl from "../../helpers/getProductUrl.js";
 
 const productInfo = async (req, res) => {
   const { url } = req.body;
+  let productUrl;
 
   try {
-    const productUrl = await getProductUrl(url);
+    productUrl = await getProductUrl(url);
 
     if (productUrl) {
       const product = await getProductData(productUrl);
       res.status(200).send({ productUrl, product });
     } else {
-      res.status(400).send({ error: "Error getting product id" });
+      res.status(400).send({ message: "Error getting productUrl", url });
     }
   } catch (error) {
-    res.status(400).send({ message: "Error getting product id", error });
+    res
+      .status(400)
+      .send({ message: "Error getting product data", url, productUrl, error });
   }
 };
 
