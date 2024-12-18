7592d83312636b6d3328fe7800c8258e760fcafa
Pedro Domínguez Aguilera <p.dominguezaguilera@ext.douglas.eu>
2024-12-18 19:29:08 +0100

    Improve fetch api method

--- helpers/fetchApi.js
+++ helpers/fetchApi.js
@@ -1,13 +1,23 @@
 const fetchApi = async (url) => {
-  const res = await fetch(url, {
-    headers: {
-      "User-Agent":
-        "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:100.0) Gecko/20100101 Firefox/100.0",
-      connection: "keep-alive",
-    },
-  });
-  const json = await res.json();
-  return json;
+  try {
+    const res = await fetch(url, {
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:100.0) Gecko/20100101 Firefox/100.0",
+        connection: "keep-alive",
+      },
+    });
+
+    if (!res.ok) {
+      throw new Error(`HTTP error! status: ${res.status}`);
+    }
+
+    const json = await res.json();
+    return json;
+  } catch (error) {
+    console.error("Error fetching API:", error);
+    throw new Error("Failed to fetch data from API");
+  }
 };
 
 export default fetchApi;
